9f2441cd2a811277f836a476089471f1ba68056b
datadavev <datadavev@users.noreply.github.com>
2018-02-26 11:14:44 -0500

    Adjusted classifiers

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
 
     # Author details
     author='Dave Vieglais',
-    author_email='',
+    author_email='datadavev@users.noreply.github.com',
 
     license='Apache 2.0',
 
@@ -34,14 +34,16 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
+
         'Topic :: Scientific/Engineering',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: Apache 2.0 License',
+        'License :: OSI Approved :: Apache Software License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
